dfb6fc19665c65587b13b6288b830b9d24e40e27
Kolpy78 <thekolp013@gmail.com>
2026-01-02 22:49:02 -0600

    first commit

---
+++ src/main/java/com/KillerStudios/ElevatorMod/ElevatorBlock.java
@@ -0,0 +1,11 @@
+package com.KillerStudios.ElevatorMod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class ElevatorBlock extends Block {
+
+    protected ElevatorBlock(Material materialIn) {
+        super(materialIn);
+    }
+}
